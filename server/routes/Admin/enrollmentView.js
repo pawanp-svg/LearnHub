a12c63d23e4a330c05a59588d59ce25e46e77334
Swathi S R <ssr@kaplaninc.com>
2025-11-12 11:31:38 +0530

    updated controller

--- server/routes/Admin/enrollmentView.js
+++ server/routes/Admin/enrollmentView.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { viewEnrollments } from "../../controllers/Admin/viewController.js";
+import { authMiddleware, isAdmin } from "../../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get("/:courseId", viewEnrollments);
+router.get("/:courseId", authMiddleware, isAdmin,viewEnrollments);
 
 export default router;
